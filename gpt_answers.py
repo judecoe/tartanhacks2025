60d438bfa4b7d751f4de257088330238120cbdd4
kiermurr <KFM41@pitt.edu>
2025-02-08 00:20:15 -0500

    comments in gpt_answers

--- gpt_answers.py
+++ gpt_answers.py
@@ -1,10 +1,13 @@
 from gpt_functions import gpt_functions
 
 class gpt_answers:
-    
-    #receieve parameters from jude
+
+    # Receieve parameters from jude
+    # quest_type is the key from the question_type dictionary in gpt_functions 
     quest_type = 
+    # question is just the string containing the question
     question = 
+    # answer_options is a dictionary containing the answer options for multiple choice questions, otherwise NONE
     answer_options = 
 
     # call function that provides answers & question type

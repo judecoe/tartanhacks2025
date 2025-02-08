32bf063304c899d125c59c92945f23a1e15f7e3c
Matthew Davis <matthewdavis207@gmail.com>
2025-02-07 23:57:37 -0500

    hi
    Co-authored-by: Kieran Murray <kiermurr@users.noreply.github.com>

--- selenium_tophat_test.py
+++ selenium_tophat_test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 # Define your existing session details
 # session_id = "7f592379-dcd1-4742-9c76-f5f033593004"  # Get this from the browser's developer tools or logs
 
@@ -20,10 +21,10 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.debugger_address = "127.0.0.1:9222"  # Ensure this matches the port you're using
 
 # # Attach to the existing session
-driver = webdriver.Remote(
-    command_executor=executor_url,
-    desired_capabilities={})
-driver.session_id = session_id
+# driver = webdriver.Remote(
+#     command_executor=executor_url,
+#     desired_capabilities={})
+# driver.session_id = session_id
 
 
 
@@ -34,16 +35,28 @@ driver = webdriver.Chrome(service=service, options=options)
 # Open the TopHat website
 driver.get("https://tophat.com")
 
-# Print the page title
-print(driver.title)
+# Access open button
+open_element = driver.find_element(By.XPATH, "//span[text()='Open']")
+
+#Go into question
+open_element.click()
 
 # Retrieve question type 
-
+quest_type = gpt_answers.get_quest_type()
+# Retrieve answers
+answer = gpt_answers.get_answers()
 # Working with webpage elements------------------
 
 
-# Find an element by its ID (change the ID to match one on the page)
-element = driver.find_element(By.CLASS_NAME, 'Labelstyles__LabelContainer')
+
+
+
+# Use case for multiple choice question
+if question_type == mult_choice:
+    element = driver.find_element(By.CLASS_NAME, f"{}")
+
+
+
 # Use case for when the question involves a text entry: element.send_keys(variable_answer + Keys.TAB + Keys.ENTER) 
 
 # Perform actions on the element (e.g., click or send text)
